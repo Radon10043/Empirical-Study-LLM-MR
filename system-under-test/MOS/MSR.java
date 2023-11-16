2eaac16514f9cfea949eccf50d735f93899a5dd5
Radon10043 <772577952@qq.com>
2023-11-16 18:04:30 +0800

    ✨ feat: Add SUT
    Add ACMS, CUBS, ERS and MOS

---
+++ system-under-test/MOS/MSR.java
@@ -0,0 +1,10 @@
+public class MSR {
+	
+	public int numberOfFirstClassMeals;	
+	public int numberOfBusinessClassMeals;	
+	public int numberOfEconomicClassMeals;
+	public int numberOfMealsForCrewMembers;
+	public int numberOfMealsForPilots;
+	public int numberOfChildMeals;
+	public int numberOfBundlesOfFlowers;
+}
