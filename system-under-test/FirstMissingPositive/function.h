14a198b22c6bb586a50937ffb8e6f73b8daab8e6
Radon10043 <772577952@qq.com>
2023-12-12 15:49:17 +0800

    🎈 perf: use vector

--- system-under-test/FirstMissingPositive/function.h
+++ system-under-test/FirstMissingPositive/function.h
@@ -2,8 +2,9 @@
 #define _HAVE_FUNCTION_H
 
 #include <iostream>
+#include <vector>
 
-int firstMissingPositive(int nums[],int n) {
+int firstMissingPositive(std::vector<int> nums,int n) {
         for (int i = 0; i < n; ++i) {
             while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
                 std::swap(nums[i], nums[nums[i] - 1]);
