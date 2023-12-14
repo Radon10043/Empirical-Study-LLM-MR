6004bd31aabf1b1fbb525502786b572a4a7dd769
Radon10043 <772577952@qq.com>
2023-12-14 14:54:52 +0800

    ✨ feat(Spwiki): Update comment

--- system-under-test/Spwiki/function.h
+++ system-under-test/Spwiki/function.h
@@ -21,28 +21,28 @@ int* Dijkstra(int matrix[][100],int source,int vertex_num)
     }
 
     
-    int min_cost;        // Ȩֵ��С
-    int min_cost_index;  // Ȩֵ��С���±�
+    int min_cost;        // 权值最小
+    int min_cost_index;  // 权值最小的下标
 
-    for (int i = 1; i < vertex_num; i++)  // �ҵ�Դ�㵽���� vertex_num-1 ��������·��
+    for (int i = 1; i < vertex_num; i++)  // 找到源点到另外 vertex_num-1 个点的最短路径
     {
         min_cost = 2147483647;
 		
         for (int j = 0; j < vertex_num; j++)
         {
-            if (visited[j] == false && dist[j] < min_cost)  // �ҵ�Ȩֵ��С
+            if (visited[j] == false && dist[j] < min_cost)  // 找到权值最小
             {
                 min_cost = dist[j];
                 min_cost_index = j;
             }
         }
         
-			visited[min_cost_index] = true;  // �õ����ҵ������б��
+			visited[min_cost_index] = true;  // 该点已找到，进行标记
 
-        for (int j = 0; j < vertex_num; j++)  // ���� dist ����
+        for (int j = 0; j < vertex_num; j++)  // 更新 dist 数组
         {
             if (visited[j] == false && min_cost != 2147483647 &&
-                matrix[min_cost_index][j] != 2147483647 &&  // ȷ������֮���б�
+                matrix[min_cost_index][j] != 2147483647 &&  // 确保两点之间有边
                 matrix[min_cost_index][j] + min_cost < dist[j])
             {
                 dist[j] = matrix[min_cost_index][j] + min_cost;
