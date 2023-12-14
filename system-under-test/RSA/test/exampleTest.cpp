85823bd67e2a479aa707c2564b92109050c4fb88
Radon10043 <772577952@qq.com>
2023-12-14 13:49:43 +0800

    🧪 test(RSA): Add test cases

---
+++ system-under-test/RSA/test/exampleTest.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+#include "../function.h"
+
+using namespace std;
+
+typedef struct RSAInput {
+    RSAInput(int m, int e, int p, int q) : m(m), e(e), p(p), q(q) {}
+    int m, e, p, q;
+} RSAInput;
+
+class RSAParamTest : public ::testing::TestWithParam<RSAInput> {};
+
+/**
+ * @brief Metamorphic Relation 1: Keeping p, q and e the same, giving a new plaintext which is equals to m + p*q, the ciphertext is the same.
+ *
+ */
+TEST_P(RSAParamTest, MR1) {
+    /* Get source input */
+    RSAInput input = GetParam();
+    int source_m = input.m, source_e = input.e, source_p = input.p, source_q = input.q;
+
+    /* Get source output */
+    int source_out = candp(source_m, source_e, source_p, source_q);
+
+    /* Construct follow-up input */
+    int follow_m = source_m + source_p * source_q;
+
+    /* Get follow-up output */
+    int follow_out = candp(follow_m, source_e, source_p, source_q);
+
+    /* Verification */
+    ASSERT_EQ(source_out, follow_out);
+}
+
+INSTANTIATE_TEST_CASE_P(TrueReturn, RSAParamTest, testing::Values(RSAInput(5, 7, 13, 17)));
