cd50f71ddd0ddaffa183625eae0cc4a02857d59f
Radon10043 <772577952@qq.com>
2024-01-05 18:01:35 +0800

    ✨ feat(Bsearch1): Update script, doc

--- software-under-test/Bsearch1/README.md
+++ software-under-test/Bsearch1/README.md
@@ -31,8 +31,20 @@ sudo make install
 
 # Run
 
-## Run specified test case
+Run specified test case:
 
 ```sh
 build/test/GPT4Test --gtest_filter=TrueReturn/BSearchParamTest.20/* > testcases.output.txt
+```
+
+Run all test cases of GPT3P5Test, each test case is repeated five times:
+
+```sh
+for i in {3..42}; do ./run.sh GPT3P5Test MR$i 5; done | tee testcases.validate.md
+```
+
+Run all test cases of GPT4Test, each test case is repeated five times:
+
+```sh
+for i in {3..42}; do ./run.sh GPT4Test MR$i 5; done | tee testcases.validate.md
 ```
--- software-under-test/Bsearch1/run.sh
+++ software-under-test/Bsearch1/run.sh
@@ -2,16 +2,23 @@
 
 main() {
     # 跑几次测试, 查看测试结果
-    echo "Testing ..."
+    echo -e "# Test $2\n"
     for ((i = 1; i <= $3; i++)); do
         $CUR_DIR/build/test/$1 --gtest_filter=TrueReturn/GetRangeParamTest.$2/* >testcases.output.txt
         if [ $? -eq 0 ]; then
-            echo "$2 Test $i success"
+            echo -e "$2 Test $i success  "
         else
-            echo "$2 Test $i failed"
+            echo -e "$2 Test $i failed  "
             break
         fi
     done
+    echo ""
+
+    # 查看测试结果输出行数, 如果小于100行, 可能是哪里写错了, 需要进行检查
+    count=$(cat testcases.output.txt | wc -l)
+    if [ $count -lt 100 ]; then
+        echo -e "Oops, maybe there are something wrong when testing $2, please check it.\n"
+    fi
 }
 
 check() {
@@ -25,9 +32,9 @@ check() {
     # 都存在的话, 通过检查
 }
 
-# $1 哪个测试
-# $2 第几个MR
-# $3 跑几次
+# $1 要运行的二进制文件名称, 默认会去./build/test/下面找
+# $2 MR的编号, 通常是MR[x], x是数字
+# $3 要运行的次数
 CUR_DIR=$(dirname $0)
 cd $CUR_DIR
 check $1 $2
