8d100f1592b9423f4d7fad8ad46933a48e961e45
Radon10043 <772577952@qq.com>
2024-01-17 13:21:22 +0800

    ✨ feat(ACMS): Update test script

--- software-under-test/ACMS/test/ACMSTestExample.java
+++ software-under-test/ACMS/test/ACMSTestExample.java
@@ -1,51 +1,17 @@
 package test;
 
-import java.io.File;
 import java.util.stream.Stream;
 
 import src.AirlinesBaggageBillingService;
 
-import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONObject;
-
-import org.apache.commons.io.FileUtils;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class ACMSTestExample {
     /**
-     * Metamorphic Relation 1: Changing the isStudent flag from false to true should not increase
-     * the fee and may decrease it in certain scenarios.
-     */
-    @ParameterizedTest
-    @MethodSource("testcaseProvider")
-    public void test1(int airClass, int area, boolean isStudent, double luggage,
-            double economicfee) {
-        if (isStudent)
-            return;
-
-        /* Get source output */
-        AirlinesBaggageBillingService ACMS = new AirlinesBaggageBillingService();
-        double source_out = ACMS.feeCalculation(airClass, area, isStudent, luggage, economicfee);
-
-        /* Construct follow-up input */
-        Boolean follow_isStudent = true;
-
-        /* Get follow-up output */
-        double follow_out =
-                ACMS.feeCalculation(airClass, area, follow_isStudent, luggage, economicfee);
-
-        /* Verification */
-        assertTrue(follow_out <= source_out);
-    }
-
-    /**
-     * Metamorphic Relation 2: If the luggage weight is 0, changing area should not affect the
+     * Metamorphic Relation 1: If the luggage weight is 0, changing area should not affect the
      * luggagefee.
      */
     @ParameterizedTest
@@ -71,7 +37,7 @@ public class ACMSTestExample {
     }
 
     /**
-     * Metamorphic Relation 3: If isStudent is false, changing the area should not affect the
+     * Metamorphic Relation 2: If isStudent is false, changing the area should not affect the
      * luggage fee.
      */
     @ParameterizedTest
@@ -104,32 +70,6 @@ public class ACMSTestExample {
      */
     public static Stream<Arguments> testcaseProvider() throws Exception {
         /* 读取存储了测试用例的json文件 */
-        File tc_file = new File("testcases" + File.separator + "testcases.json");
-        String json_text = FileUtils.readFileToString(tc_file, "UTF-8");
-        JSONObject json_data = JSON.parseObject(json_text);
-
-        /* 统计测试用例数量 */
-        int n = json_data.size();
-        Arguments[] testcases = new Arguments[n];
-
-        /* 遍历所有测试用例并存储至testcases */
-        for (int i = 0; i < n; i++) {
-
-            int airClass, area;
-            boolean isStudent;
-            double luggage, economicfee;
-
-            JSONObject data = JSON.parseObject(json_data.get(String.valueOf(i)).toString());
-            airClass = data.getIntValue("airClass");
-            area = data.getIntValue("area");
-            isStudent = data.getBooleanValue("isStudent");
-            luggage = data.getDoubleValue("luggage");
-            economicfee = data.getDoubleValue("economicfee");
-
-            testcases[i] = Arguments.of(airClass, area, isStudent, luggage, economicfee);
-
-        }
-
-        return Stream.of(testcases);
+        return testcaseGenerator.generate(1000);
     }
 }
---
+++ software-under-test/ACMS/test/testcaseGenerator.java
@@ -0,0 +1,52 @@
+package test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+public class testcaseGenerator {
+    /**
+     * 随机生成num个测试用例, 并保存到文件中
+     *
+     * @param num
+     * @return
+     * @throws IOException
+     */
+    public static Stream<Arguments> generate(int num) throws IOException {
+        int tcs_num = num;
+        int[] range1 = {0, 1001};
+        double[] range2 = {0, 1001};
+        SecureRandom rand = new SecureRandom();
+        Arguments[] tcs = new Arguments[tcs_num];
+
+        /* 随机构建一定数量的测试用例 */
+        for (int i = 0; i < tcs_num; i++) {
+            int airClass, area;
+            boolean isStudent;
+            double luggage, economicFee;
+
+            airClass = rand.nextInt(range1[0], range1[1]);
+            area = rand.nextInt(range1[0], range1[1]);
+            isStudent = rand.nextBoolean();
+            luggage = rand.nextDouble(range2[0], range2[1]);
+            economicFee = rand.nextDouble(range2[0], range2[1]);
+            tcs[i] = Arguments.of(airClass, area, isStudent, luggage, economicFee);
+        }
+
+        /* 将测试用例写入文件, 方便调试 */
+        File file = new File("testcases.txt");
+        FileWriter writer = new FileWriter(file);
+        for (int i = 0; i < tcs_num; i++) {
+            writer.write("Testcase " + String.valueOf(i) + ":\n");
+            writer.write("----------------------------------------------------------------\n");
+            writer.write(tcs[i].toString() + "\n");
+        }
+        writer.close();
+
+        return Stream.of(tcs);
+    }
+}
