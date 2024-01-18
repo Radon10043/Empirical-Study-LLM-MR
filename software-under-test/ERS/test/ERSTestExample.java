53bc35abfd2ac66ed56705ddedadc5cd361eded2
Radon10043 <772577952@qq.com>
2024-01-18 16:49:03 +0800

    ✨ feat(ERS): Update scripts

--- software-under-test/ERS/test/ERSTestExample.java
+++ software-under-test/ERS/test/ERSTestExample.java
@@ -1,14 +1,11 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.io.File;
 import java.util.stream.Stream;
-import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONObject;
+
 
 import src.ExpenseReimbursementSystem;
 
@@ -71,33 +68,6 @@ public class ERSTestExample {
      * @throws Exception
      */
     public static Stream<Arguments> testcaseProvider() throws Exception {
-        /* 读取存储了测试用例的json文件 */
-        File tc_file = new File("testcases" + File.separator + "testcases.json");
-        String json_text = FileUtils.readFileToString(tc_file, "UTF-8");
-        JSONObject json_data = JSON.parseObject(json_text);
-
-        /* 统计测试用例数量 */
-        int n = json_data.size();
-        Arguments[] testcases = new Arguments[n];
-
-        /* 遍历所有测试用例并存储至testcases */
-        for (int i = 0; i < n; i++) {
-
-            String stafflevel;
-            double actualmonthlymileage, monthlysalesamount, airfareamount, otherexpensesamount;
-
-            JSONObject data = JSON.parseObject(json_data.get(String.valueOf(i)).toString());
-            stafflevel = data.getString("stafflevel");
-            actualmonthlymileage = data.getDoubleValue("actualmonthlymileage");
-            monthlysalesamount = data.getDoubleValue("monthlysalesamount");
-            airfareamount = data.getDoubleValue("airfareamount");
-            otherexpensesamount = data.getDoubleValue("otherexpensesamount");
-
-            testcases[i] = Arguments.of(stafflevel, actualmonthlymileage, monthlysalesamount,
-                    airfareamount, otherexpensesamount);
-
-        }
-
-        return Stream.of(testcases);
+        return testcaseGenerator.generate(1000);
     }
 }
---
+++ software-under-test/ERS/test/testcaseGenerator.java
@@ -0,0 +1,54 @@
+package test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+public class testcaseGenerator {
+    /**
+     * 随机生成num个测试用例, 并保存到文件中
+     *
+     * @param num
+     * @return
+     * @throws IOException
+     */
+    public static Stream<Arguments> generate(int num) throws IOException {
+        int tcs_num = num;
+        String[] cand = { "seniormanager", "manager", "supervisor" };
+        int[] range = {1000, 100000};
+        SecureRandom rand = new SecureRandom();
+        Arguments[] tcs = new Arguments[tcs_num];
+
+        /* 随机构建一定数量的测试用例 */
+        for (int i = 0; i < tcs_num; i++) {
+            String stafflevel;
+            double actualmonthlymileage, monthlysalesamount, airfareamount, otherexpensesamount;
+            stafflevel = cand[rand.nextInt(cand.length)];
+            actualmonthlymileage = rand.nextInt(range[1] - range[0]) + range[0];
+            monthlysalesamount = rand.nextInt(range[1] - range[0]) + range[0];
+            airfareamount = rand.nextInt(range[1] - range[0]) + range[0];
+            otherexpensesamount = rand.nextInt(range[1] - range[0]) + range[0];
+            tcs[i] = Arguments.of(stafflevel, actualmonthlymileage, monthlysalesamount, airfareamount, otherexpensesamount);
+        }
+
+        /* 将测试用例写入文件, 方便调试 */
+        File file = new File("testcases.txt");
+        FileWriter writer = new FileWriter(file);
+        for (int i = 0; i < tcs_num; i++) {
+            writer.write("Testcase " + String.valueOf(i) + ":\n");
+            writer.write("----------------------------------------------------------------\n");
+            writer.write("stafflevel          : " + tcs[i].get()[0].toString() + "\n");
+            writer.write("actualmonthlymileage: " + tcs[i].get()[1].toString() + "\n");
+            writer.write("monthlysalesamount  : " + tcs[i].get()[2].toString() + "\n");
+            writer.write("airfareamount       : " + tcs[i].get()[3].toString() + "\n");
+            writer.write("otherexpenseamount  : " + tcs[i].get()[4].toString() + "\n\n");
+        }
+        writer.close();
+
+        return Stream.of(tcs);
+    }
+}
