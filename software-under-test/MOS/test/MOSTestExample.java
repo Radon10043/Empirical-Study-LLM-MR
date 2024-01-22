162dc04fa7cd53729a2d19fd6e4dd1beb1bd5142
Radon10043 <772577952@qq.com>
2024-01-22 17:10:45 +0800

    ✨ feat(MOS): Update
    Update prompt & example

--- software-under-test/MOS/test/MOSTestExample.java
+++ software-under-test/MOS/test/MOSTestExample.java
@@ -39,43 +39,6 @@ public class MOSTestExample {
     }
 
     /**
-     * Metamorphic Relation 2: If the aircraftmodel parameter remains the same while other
-     * parameters change, the values related to seat configuration (numberOfFirstClassMeals,
-     * numberOfBusinessClassMeals, numberOfEconomicClassMeals) should remain constant.
-     */
-    @ParameterizedTest
-    @MethodSource("testcaseProvider")
-    public void test2(String aircraftmodel, String changeinthenumberofcrewmembers,
-            int newnumberofcrewmembers, String changeinthenumberofpilots, int newnumberofpilots,
-            int numberofchildpassengers, int numberofrequestedbundlesofflowers) {
-        /* Get source output */
-        MealOrderingSystem MOS = new MealOrderingSystem();
-        MSR source_out = MOS.generateMSR(aircraftmodel, changeinthenumberofcrewmembers,
-                newnumberofcrewmembers, changeinthenumberofpilots, newnumberofpilots,
-                numberofchildpassengers, numberofrequestedbundlesofflowers);
-
-        /* Construct follow-up input */
-        String follow_changeinthenumberofpilots;
-        if (changeinthenumberofpilots.equals("y")) {
-            follow_changeinthenumberofpilots = "n";
-        } else {
-            follow_changeinthenumberofpilots = "y";
-        }
-        int follow_newnumberofpilots = newnumberofpilots * 2;
-        int follow_numberofchildpassengers = numberofchildpassengers * 2;
-
-        /* Get follow-up output */
-        MSR follow_out = MOS.generateMSR(aircraftmodel, changeinthenumberofcrewmembers,
-                newnumberofcrewmembers, follow_changeinthenumberofpilots, follow_newnumberofpilots,
-                follow_numberofchildpassengers, numberofrequestedbundlesofflowers);
-
-        /* Verification */
-        assertTrue(source_out.numberOfFirstClassMeals == follow_out.numberOfFirstClassMeals);
-        assertTrue(source_out.numberOfBusinessClassMeals == follow_out.numberOfBusinessClassMeals);
-        assertTrue(source_out.numberOfEconomicClassMeals == follow_out.numberOfEconomicClassMeals);
-    }
-
-    /**
      * 随机生成测试用例
      *
      * @return
