47a5250b2442e19b059e2a970d7320f881d5e955
Radon10043 <772577952@qq.com>
2024-01-24 21:47:22 +0800

    ✨ feat(rotate): Update
    Update prompt & example

--- software-under-test/rotate/test/example_test.py
+++ software-under-test/rotate/test/example_test.py
@@ -2,7 +2,7 @@
 Author: Radon
 Date: 2023-12-20 21:42:37
 LastEditors: Radon
-LastEditTime: 2023-12-21 09:58:50
+LastEditTime: 2024-01-24 21:46:07
 Description: Hi, say something
 """
 import unittest
@@ -10,8 +10,8 @@ import unittest
 import numpy as np
 
 from parameterized import parameterized
-from scipy import ndimage, datasets
-
+from scipy import ndimage
+from generate import *
 
 def load_test_cases() -> list:
     """加载测试用例
@@ -25,7 +25,7 @@ def load_test_cases() -> list:
     -----
     _description_
     """
-    return [(datasets.ascent(), 45), (datasets.face(), 60)]
+    return gen_tcs_randomly()
 
 
 class TestingClass(unittest.TestCase):
@@ -33,13 +33,13 @@ class TestingClass(unittest.TestCase):
     def test1(self, img: np.array, angle: float):
         """Metamorphic Relation 1: Rotating the same image by N degree and N+360 degree will output the same result."""
         # Get source output
-        source_out = ndimage.rotate(img, angle)
+        source_out = ndimage.rotate(img, angle, reshape=False)
 
         # Construct follow-up input
         follow_angle = angle + 360
 
         # Get follow-up output
-        follow_out = ndimage.rotate(img, follow_angle)
+        follow_out = ndimage.rotate(img, follow_angle, reshape=False)
 
         # Verification
         self.assertTrue(np.any(follow_out - source_out) == 0)
---
+++ software-under-test/rotate/test/generate.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+import numpy as np
+
+# fmt:off
+# ========== GLOBAL VARIABLES =========
+TESTCASE_NUM = 10     # 测试用例的数量
+IMAGE_HEIGHT = 256      # 图像高度
+IMAGE_WIDTH = 256       # 图像宽度
+RANGE_ANGLE = (-360, 360)   # 旋转角度的范围
+# =====================================
+# fmt:on
+
+
+def gen_tcs_randomly() -> list:
+    """随机生成一定数量的测试用例
+
+    Returns
+    -------
+    list
+        存储测试用例的列表
+    """
+    testcases = list()
+    for i in range(TESTCASE_NUM):
+        print("\rGenerating testcases: {}/{}".format(i + 1, TESTCASE_NUM), end="")
+        img = np.random.randint(0, 256, size=(IMAGE_HEIGHT, IMAGE_WIDTH, 3), dtype=np.uint8)
+        angle = np.random.uniform(*RANGE_ANGLE)
+        testcases.append((img, angle))
+    print("\nDone!")
+    return testcases
