b5730d71a17e53a24e30b201119edb226ed5a1a4
Radon10043 <772577952@qq.com>
2024-02-05 10:24:36 +0800

    ✨ feat(Boyer): Update examples

--- software-under-test/Boyer/test/BoyerTestExample.java
+++ software-under-test/Boyer/test/BoyerTestExample.java
@@ -11,23 +11,52 @@ import src.Boyer;
 
 public class BoyerTestExample {
     /**
-     * Metamorphic Relation 1: Add a string to the end of the origin string, the
-     * output of the algorithm should be same or greater.
+     * Metamorphic Relation 1: If the pattern occurred in the original text, when adding one
+     * character to the end of the source text, the output should be the same.
      */
     @ParameterizedTest
     @MethodSource("testcaseProvider")
     public void test1(String text, String pattern) {
+        if (text.indexOf(pattern) == -1) {
+            return;
+        }
+
         /* Get origin output */
         int origin_res = Boyer.indexOf(text, pattern);
 
         /* Construct follow-up input */
-        String follow_text = text + "xxx";
+        String follow_text = text + "x";
 
         /* Get follow-up output */
         int follow_res = Boyer.indexOf(follow_text, pattern);
 
         /* Verification */
-        assertTrue(follow_res >= origin_res);
+        assertTrue(follow_res == origin_res);
+    }
+
+    /**
+     * Metamorphic Relation 2: If the pattern occurred in the original text, when deleting one
+     * character from the end of the pattern, the output should be the same as or less than the
+     * original output.
+     */
+    @ParameterizedTest
+    @MethodSource("testcaseProvider")
+    public void test2(String text, String pattern) {
+        if (text.indexOf(pattern) == -1) {
+            return;
+        }
+
+        /* Get origin output */
+        int origin_res = Boyer.indexOf(text, pattern);
+
+        /* Construct follow-up input */
+        String follow_text = text.substring(0, text.length() - 1);
+
+        /* Get follow-up output */
+        int follow_res = Boyer.indexOf(follow_text, pattern);
+
+        /* Verification */
+        assertTrue(follow_res <= origin_res);
     }
 
     static Stream<Arguments> testcaseProvider() throws IOException {
