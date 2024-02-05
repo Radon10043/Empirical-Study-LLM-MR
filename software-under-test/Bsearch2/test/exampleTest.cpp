60017cf430195dce8f1f030e25c95bf79164525f
Radon10043 <772577952@qq.com>
2024-02-05 12:06:41 +0800

    ✨ feat(Bsearch2): Update example

--- software-under-test/Bsearch2/test/exampleTest.cpp
+++ software-under-test/Bsearch2/test/exampleTest.cpp
@@ -9,38 +9,10 @@ using namespace std;
 class BSearchParamTest : public ::testing::TestWithParam<BSearch2Input> {};
 
 /**
- * @brief Metamorphic relation 1: If the target value is already in the array, add an element greater than the max value in array to the end of the array, and
- * the output should be unchanged.
+ * @brief Metamorphic relation 1: Multiplying all the numbers (including the numbers in the array and the searching number), then the output should be the same.
  *
  */
 TEST_P(BSearchParamTest, MR1) {
-    /* Get source input */
-    BSearch2Input input = GetParam();
-    vector<int> vec = input.vec;
-    int target = input.target;
-
-    /* Get source output */
-    int source_out = bin_search(vec, target);
-
-    if (!source_out)
-        return;
-
-    /* Construct follow-up input */
-    vector<int> follow_vec = vec;
-    follow_vec.emplace_back(vec.back() + 1);
-
-    /* Get follow-up output */
-    int follow_out = bin_search(follow_vec, target);
-
-    /* Verification */
-    EXPECT_EQ(source_out, follow_out);
-}
-
-/**
- * @brief Metamorphic relation 2: Multiply all elements in the array and the element to be located by a constant, the output should remain the same.
- *
- */
-TEST_P(BSearchParamTest, MR2) {
     /* Get source input */
     BSearch2Input input = GetParam();
     vector<int> vec = input.vec;
