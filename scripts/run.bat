239b8b3ff4a28ff8682d08c05ff193b21db377db
Radon10043 <772577952@qq.com>
2024-02-05 17:58:03 +0800

    ✨ feat(Scripts): Add run.bat

---
+++ scripts/run.bat
@@ -0,0 +1,75 @@
+chcp 65001
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.ACMS.md --demo .\data\demo\demo.ACMS.md --model gpt-3.5-turbo-1106 --count 30 --name ACMS
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Boyer.md --demo .\data\demo\demo.Boyer.md --model gpt-3.5-turbo-1106 --count 30 --name Boyer
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Bsearch1.md --demo .\data\demo\demo.Bsearch1.md --model gpt-3.5-turbo-1106 --count 30 --name Bsearch1
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Bsearch2.md --demo .\data\demo\demo.Bsearch2.md --model gpt-3.5-turbo-1106 --count 50 --name Bsearch2
+
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.ClosestPair.md --demo .\data\demo\demo.ClosestPair.md --model gpt-3.5-turbo-1106 --count 30 --name ClosestPair
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.CUBS.md --demo .\data\demo\demo.CUBS.md --model gpt-3.5-turbo-1106 --count 30 --name CUBS
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Determinant1.md --demo .\data\demo\demo.Determinant1.md --model gpt-3.5-turbo-1106 --count 30 --name Determinant1
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.DistinctSubsequence.md --demo .\data\demo\demo.DistinctSubsequence.md --model gpt-3.5-turbo-1106 --count 30 --name DistinctSubsequence
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Editingdistance.md --demo .\data\demo\demo.Editingdistance.md --model gpt-3.5-turbo-1106 --count 30 --name Editingdistance
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.ERS.md --demo .\data\demo\demo.ERS.md --model gpt-3.5-turbo-1106 --count 30 --name ERS
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.f_oneway.md --demo .\data\demo\demo.f_oneway.md --model gpt-3.5-turbo-1106 --count 30 --name f_oneway
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.FastJson.md --demo .\data\demo\demo.FastJson.md --model gpt-3.5-turbo-1106 --count 30 --name FastJson
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.FirstMissingPositive.md --demo .\data\demo\demo.FirstMissingPositive.md --model gpt-3.5-turbo-1106 --count 30 --name FirstMissingPositive
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.grep.md --demo .\data\demo\demo.grep.md --model gpt-3.5-turbo-1106 --count 30 --name grep
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.HeapSort.md --demo .\data\demo\demo.HeapSort.md --model gpt-3.5-turbo-1106 --count 30 --name HeapSort
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.InterleavingString.md --demo .\data\demo\demo.InterleavingString.md --model gpt-3.5-turbo-1106 --count 30 --name InterleavingString
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.JAMA.md --demo .\data\demo\demo.JAMA.md --model gpt-3.5-turbo-1106 --count 30 --name JAMA
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Jodatime.md --demo .\data\demo\demo.Jodatime.md --model gpt-3.5-turbo-1106 --count 30 --name Jodatime
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Klp.md --demo .\data\demo\demo.Klp.md --model gpt-3.5-turbo-1106 --count 30 --name Klp
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Lucene.md --demo .\data\demo\demo.Lucene.md --model gpt-3.5-turbo-1106 --count 30 --name Lucene
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.MaxRectangle.md --demo .\data\demo\demo.MaxRectangle.md --model gpt-3.5-turbo-1106 --count 30 --name MaxRectangle
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Maxsub.md --demo .\data\demo\demo.Maxsub.md --model gpt-3.5-turbo-1106 --count 30 --name Maxsub
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.MOS.md --demo .\data\demo\demo.MOS.md --model gpt-3.5-turbo-1106 --count 30 --name MOS
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.MultiM-MAXSUM.md --demo .\data\demo\demo.Multi-MAXSUM.md --model gpt-3.5-turbo-1106 --count 30 --name Multi-MAXSUM
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Printtokens.md --demo .\data\demo\demo.Printtokens.md --model gpt-3.5-turbo-1106 --count 30 --name Printtokens
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Printtokens2.md --demo .\data\demo\demo.Printtokens2.md --model gpt-3.5-turbo-1106 --count 30 --name Printtokens2
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.QuickSort.md --demo .\data\demo\demo.QuickSort.md --model gpt-3.5-turbo-1106 --count 30 --name QuickSort
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.rotate.md --demo .\data\demo\demo.rotate.md --model gpt-3.5-turbo-1106 --count 30 --name rotate
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.RSA.md --demo .\data\demo\demo.RSA.md --model gpt-3.5-turbo-1106 --count 30 --name RSA
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.schedule.md --demo .\data\demo\demo.schedule.md --model gpt-3.5-turbo-1106 --count 30 --name schedule
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.schedule2.md --demo .\data\demo\demo.schedule2.md --model gpt-3.5-turbo-1106 --count 30 --name schedule2
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.shortest_path.md --demo .\data\demo\demo.shortest_path.md --model gpt-3.5-turbo-1106 --count 30 --name shortest_path
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Spwiki.md --demo .\data\demo\demo.Spwiki.md --model gpt-3.5-turbo-1106 --count 30 --name Spwiki
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Superstring.md --demo .\data\demo\demo.Superstring.md --model gpt-3.5-turbo-1106 --count 30 --name Superstring
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.SurroundedRegion.md --demo .\data\demo\demo.SurroundedRegion.md --model gpt-3.5-turbo-1106 --count 30 --name SurroundedRegion
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.TCAS.md --demo .\data\demo\demo.TCAS.md --model gpt-3.5-turbo-1106 --count 30 --name TCAS
+
+@REM venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Trisquarej.md --demo .\data\demo\demo.Trisquarej.md --model gpt-3.5-turbo-1106 --count 30 --name Trisquarej
