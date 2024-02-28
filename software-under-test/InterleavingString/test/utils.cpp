cf68b16c68373946d14bcd7078049b9dffa9c57c
Radon10043 <772577952@qq.com>
2024-02-28 12:17:25 +0800

    ✨ feat(InterleavingString): Update utils

--- software-under-test/InterleavingString/test/utils.cpp
+++ software-under-test/InterleavingString/test/utils.cpp
@@ -12,13 +12,14 @@ vector<InterleavingStringInput> gen_tcs_randomly() {
     mt19937 rng(random_device{}());
 
     /* clang-format off */
-    uniform_int_distribution<int> dist_size(1, 1000),    /* 数据长度范围 */
-                                  dist_value(0,25);
+    uniform_int_distribution<int> dist_size(1, 10),    /* 数据长度范围 */
+                                  dist_value(0, 25),     /* 字符范围 */
+                                  dist_prob(1, 100);     /* 交错概率 */
     /* clang-format on */
 
     /* 构建测试用例 */
     for (int i = 0; i < TESTCASE_NUM; i++) {
-        int size_A = dist_size(rng), size_B = dist_size(rng), size_C = dist_size(rng);
+        int size_A = dist_size(rng), size_B = dist_size(rng);
         string A = "", B = "", C = "";
         for (int j = 0; j < size_A; j++) {
             A += 'a' + dist_value(rng);
@@ -26,8 +27,20 @@ vector<InterleavingStringInput> gen_tcs_randomly() {
         for (int j = 0; j < size_B; j++) {
             B += 'a' + dist_value(rng);
         }
-        for (int j = 0; j < size_C; j++) {
-            C += 'a' + dist_value(rng);
+
+        /* 生成交错字符串和随机字符串的概率分别是50% */
+        bool interleave = dist_prob(rng) > 50;
+        if (interleave) {
+            int i = 0, j = 0;
+            while (i < size_A && j < size_B) {
+                C += A[i++];
+                C += B[j++];
+            }
+        } else {
+            int size_C = dist_size(rng);
+            for (int j = 0; j < size_C; j++) {
+                C += 'a' + dist_value(rng);
+            }
         }
         tcs.push_back(InterleavingStringInput(A, B, C));
     }
