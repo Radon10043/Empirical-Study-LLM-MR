8afc3e33db6d22e6e440132e91da1375da3c552a
Radon10043 <772577952@qq.com>
2024-05-11 15:35:53 +0800

    ✨ feat: Update run.bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,75 +1,58 @@
 chcp 65001
 
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.ACMS.md --demo .\data\demo\demo.ACMS.md --model gpt-3.5-turbo-1106 --name ACMS
+@REM Vanilla
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.ACMS.md                 --demo .\data\demo\demo.ACMS.md                 --model gpt-4-1106-preview --name ACMS
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Boyer.md                --demo .\data\demo\demo.Boyer.md                --model gpt-4-1106-preview --name Boyer
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Bsearch1.md             --demo .\data\demo\demo.Bsearch1.md             --model gpt-4-1106-preview --name Bsearch1
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Bsearch2.md             --demo .\data\demo\demo.Bsearch2.md             --model gpt-4-1106-preview --name Bsearch2
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.ClosestPair.md          --demo .\data\demo\demo.ClosestPair.md          --model gpt-4-1106-preview --name ClosestPair
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.CUBS.md                 --demo .\data\demo\demo.CUBS.md                 --model gpt-4-1106-preview --name CUBS
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Determinant1.md         --demo .\data\demo\demo.Determinant1.md         --model gpt-4-1106-preview --name Determinant1
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.DistinctSubsequence.md  --demo .\data\demo\demo.DistinctSubsequence.md  --model gpt-4-1106-preview --name DistinctSubsequence
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Editingdistance.md      --demo .\data\demo\demo.Editingdistance.md      --model gpt-4-1106-preview --name Editingdistance
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.ERS.md                  --demo .\data\demo\demo.ERS.md                  --model gpt-4-1106-preview --name ERS
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.f_oneway.md             --demo .\data\demo\demo.f_oneway.md             --model gpt-4-1106-preview --name f_oneway
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.FastJson.md             --demo .\data\demo\demo.FastJson.md             --model gpt-4-1106-preview --name FastJson
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.FirstMissingPositive.md --demo .\data\demo\demo.FirstMissingPositive.md --model gpt-4-1106-preview --name FirstMissingPositive
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.grep.md                 --demo .\data\demo\demo.grep.md                 --model gpt-4-1106-preview --name grep
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.HeapSort.md             --demo .\data\demo\demo.HeapSort.md             --model gpt-4-1106-preview --name HeapSort
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.InterleavingString.md   --demo .\data\demo\demo.InterleavingString.md   --model gpt-4-1106-preview --name InterleavingString
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.JAMA.md                 --demo .\data\demo\demo.JAMA.md                 --model gpt-4-1106-preview --name JAMA
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Jodatime.md             --demo .\data\demo\demo.Jodatime.md             --model gpt-4-1106-preview --name Jodatime
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Klp.md                  --demo .\data\demo\demo.Klp.md                  --model gpt-4-1106-preview --name Klp
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Lucene.md               --demo .\data\demo\demo.Lucene.md               --model gpt-4-1106-preview --name Lucene
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.MaxRectangle.md         --demo .\data\demo\demo.MaxRectangle.md         --model gpt-4-1106-preview --name MaxRectangle
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Maxsub.md               --demo .\data\demo\demo.Maxsub.md               --model gpt-4-1106-preview --name Maxsub
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.MOS.md                  --demo .\data\demo\demo.MOS.md                  --model gpt-4-1106-preview --name MOS
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Multi-MAXSUM.md         --demo .\data\demo\demo.Multi-MAXSUM.md         --model gpt-4-1106-preview --name Multi-MAXSUM
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Printtokens.md          --demo .\data\demo\demo.Printtokens.md          --model gpt-4-1106-preview --name Printtokens
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Printtokens2.md         --demo .\data\demo\demo.Printtokens2.md         --model gpt-4-1106-preview --name Printtokens2
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.QuickSort.md            --demo .\data\demo\demo.QuickSort.md            --model gpt-4-1106-preview --name QuickSort
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.rotate.md               --demo .\data\demo\demo.rotate.md               --model gpt-4-1106-preview --name rotate
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.RSA.md                  --demo .\data\demo\demo.RSA.md                  --model gpt-4-1106-preview --name RSA
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.schedule.md             --demo .\data\demo\demo.schedule.md             --model gpt-4-1106-preview --name schedule
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.schedule2.md            --demo .\data\demo\demo.schedule2.md            --model gpt-4-1106-preview --name schedule2
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.shortest_path.md        --demo .\data\demo\demo.shortest_path.md        --model gpt-4-1106-preview --name shortest_path
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Spwiki.md               --demo .\data\demo\demo.Spwiki.md               --model gpt-4-1106-preview --name Spwiki
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Superstring.md          --demo .\data\demo\demo.Superstring.md          --model gpt-4-1106-preview --name Superstring
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.SurroundedRegion.md     --demo .\data\demo\demo.SurroundedRegion.md     --model gpt-4-1106-preview --name SurroundedRegion
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.TCAS.md                 --demo .\data\demo\demo.TCAS.md                 --model gpt-4-1106-preview --name TCAS
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Trisquarej.md           --demo .\data\demo\demo.Trisquarej.md           --model gpt-4-1106-preview --name Trisquarej
 
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Boyer.md --demo .\data\demo\demo.Boyer.md --model gpt-3.5-turbo-1106 --name Boyer
+@REM No System
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.nosystem.md --spec .\data\specification\specification.Bsearch2.md             --demo .\data\demo\demo.Bsearch2.md             --model gpt-4-1106-preview --name Bsearch2
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.nosystem.md --spec .\data\specification\specification.Boyer.md                --demo .\data\demo\demo.Boyer.md                --model gpt-4-1106-preview --name Boyer
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.nosystem.md --spec .\data\specification\specification.QuickSort.md            --demo .\data\demo\demo.QuickSort.md            --model gpt-4-1106-preview --name QuickSort
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.nosystem.md --spec .\data\specification\specification.shortest_path.md        --demo .\data\demo\demo.shortest_path.md        --model gpt-4-1106-preview --name shortest_path
 
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Bsearch1.md --demo .\data\demo\demo.Bsearch1.md --model gpt-3.5-turbo-1106 --name Bsearch1
+@REM No Specification
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.nospec.md   --spec .\data\specification\specification.Bsearch2.md             --demo .\data\demo\demo.Bsearch2.md             --model gpt-4-1106-preview --name Bsearch2
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.nospec.md   --spec .\data\specification\specification.Boyer.md                --demo .\data\demo\demo.Boyer.md                --model gpt-4-1106-preview --name Boyer
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.nospec.md   --spec .\data\specification\specification.QuickSort.md            --demo .\data\demo\demo.QuickSort.md            --model gpt-4-1106-preview --name QuickSort
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.nospec.md   --spec .\data\specification\specification.shortest_path.md        --demo .\data\demo\demo.shortest_path.md        --model gpt-4-1106-preview --name shortest_path
 
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Bsearch2.md --demo .\data\demo\demo.Bsearch2.md --model gpt-3.5-turbo-1106 --name Bsearch2
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.ClosestPair.md --demo .\data\demo\demo.ClosestPair.md --model gpt-3.5-turbo-1106 --name ClosestPair
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.CUBS.md --demo .\data\demo\demo.CUBS.md --model gpt-3.5-turbo-1106 --name CUBS
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Determinant1.md --demo .\data\demo\demo.Determinant1.md --model gpt-3.5-turbo-1106 --name Determinant1
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.DistinctSubsequence.md --demo .\data\demo\demo.DistinctSubsequence.md --model gpt-3.5-turbo-1106 --name DistinctSubsequence
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Editingdistance.md --demo .\data\demo\demo.Editingdistance.md --model gpt-3.5-turbo-1106 --name Editingdistance
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.ERS.md --demo .\data\demo\demo.ERS.md --model gpt-3.5-turbo-1106 --name ERS
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.f_oneway.md --demo .\data\demo\demo.f_oneway.md --model gpt-3.5-turbo-1106 --name f_oneway
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.FastJson.md --demo .\data\demo\demo.FastJson.md --model gpt-3.5-turbo-1106 --name FastJson
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.FirstMissingPositive.md --demo .\data\demo\demo.FirstMissingPositive.md --model gpt-3.5-turbo-1106 --name FirstMissingPositive
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.grep.md --demo .\data\demo\demo.grep.md --model gpt-3.5-turbo-1106 --name grep
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.HeapSort.md --demo .\data\demo\demo.HeapSort.md --model gpt-3.5-turbo-1106 --name HeapSort
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.InterleavingString.md --demo .\data\demo\demo.InterleavingString.md --model gpt-3.5-turbo-1106 --name InterleavingString
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.JAMA.md --demo .\data\demo\demo.JAMA.md --model gpt-3.5-turbo-1106 --name JAMA
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Jodatime.md --demo .\data\demo\demo.Jodatime.md --model gpt-3.5-turbo-1106 --name Jodatime
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Klp.md --demo .\data\demo\demo.Klp.md --model gpt-3.5-turbo-1106 --name Klp
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Lucene.md --demo .\data\demo\demo.Lucene.md --model gpt-3.5-turbo-1106 --name Lucene
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.MaxRectangle.md --demo .\data\demo\demo.MaxRectangle.md --model gpt-3.5-turbo-1106 --name MaxRectangle
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Maxsub.md --demo .\data\demo\demo.Maxsub.md --model gpt-3.5-turbo-1106 --name Maxsub
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.MOS.md --demo .\data\demo\demo.MOS.md --model gpt-3.5-turbo-1106 --name MOS
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Multi-MAXSUM.md --demo .\data\demo\demo.Multi-MAXSUM.md --model gpt-3.5-turbo-1106 --name Multi-MAXSUM
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Printtokens.md --demo .\data\demo\demo.Printtokens.md --model gpt-3.5-turbo-1106 --name Printtokens
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Printtokens2.md --demo .\data\demo\demo.Printtokens2.md --model gpt-3.5-turbo-1106 --name Printtokens2
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.QuickSort.md --demo .\data\demo\demo.QuickSort.md --model gpt-3.5-turbo-1106 --name QuickSort
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.rotate.md --demo .\data\demo\demo.rotate.md --model gpt-3.5-turbo-1106 --name rotate
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.RSA.md --demo .\data\demo\demo.RSA.md --model gpt-3.5-turbo-1106 --name RSA
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.schedule.md --demo .\data\demo\demo.schedule.md --model gpt-3.5-turbo-1106 --name schedule
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.schedule2.md --demo .\data\demo\demo.schedule2.md --model gpt-3.5-turbo-1106 --name schedule2
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.shortest_path.md --demo .\data\demo\demo.shortest_path.md --model gpt-3.5-turbo-1106 --name shortest_path
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Spwiki.md --demo .\data\demo\demo.Spwiki.md --model gpt-3.5-turbo-1106 --name Spwiki
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Superstring.md --demo .\data\demo\demo.Superstring.md --model gpt-3.5-turbo-1106 --name Superstring
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.SurroundedRegion.md --demo .\data\demo\demo.SurroundedRegion.md --model gpt-3.5-turbo-1106 --name SurroundedRegion
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.TCAS.md --demo .\data\demo\demo.TCAS.md --model gpt-3.5-turbo-1106 --name TCAS
-
-venv\Scripts\python.exe chat.py --template .\data\template\prompt.template.md --spec .\data\specification\specification.Trisquarej.md --demo .\data\demo\demo.Trisquarej.md --model gpt-3.5-turbo-1106 --name Trisquarej
+@REM No Demo
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.nodemo.md   --spec .\data\specification\specification.Bsearch2.md             --demo .\data\demo\demo.Bsearch2.md             --model gpt-4-1106-preview --name Bsearch2
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.nodemo.md   --spec .\data\specification\specification.Boyer.md                --demo .\data\demo\demo.Boyer.md                --model gpt-4-1106-preview --name Boyer
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.nodemo.md   --spec .\data\specification\specification.QuickSort.md            --demo .\data\demo\demo.QuickSort.md            --model gpt-4-1106-preview --name QuickSort
+venv\Scripts\python.exe chat.py --template .\data\template\prompt.nodemo.md   --spec .\data\specification\specification.shortest_path.md        --demo .\data\demo\demo.shortest_path.md        --model gpt-4-1106-preview --name shortest_path
