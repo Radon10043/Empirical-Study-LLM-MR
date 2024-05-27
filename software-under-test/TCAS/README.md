589cbca57a41e5742a83ec882376d3ec5db14571
Radon10043 <772577952@qq.com>
2024-05-27 11:17:45 +0800

    🦄 refactor(TCAS): Update

--- software-under-test/TCAS/README.md
+++ software-under-test/TCAS/README.md
@@ -4,6 +4,14 @@ python 3.8.3
 
 # Run tests
 
+For exmaple:
+
+```sh
+for i in {1..4}; do ./run.sh example MR$i 5; done | tee testcases.validate.md
+```
+
+For GPT3.5:
+
 ```sh
 for i in {5..44}; do ./run.sh GPT3D5 MR$i 5; done | tee testcases.validate.md
 ```
--- software-under-test/TCAS/test/utils.py
+++ software-under-test/TCAS/test/utils.py
@@ -2,13 +2,18 @@
 Author: Radon
 Date: 2024-01-10 21:23:15
 LastEditors: Radon
-LastEditTime: 2024-02-14 13:52:20
+LastEditTime: 2024-05-27 11:09:07
 Description: Hi, say something
 """
 import os
+import unittest
+import subprocess
+import sys
+import shutil
+
+from parameterized import parameterized
 
 from random import randint
-import shutil
 
 
 # fmt:off
@@ -19,6 +24,33 @@ RANGE_ALT_LAYER_VALUE  = (0, 3)
 RANGE_OTEHER_RAC       = (0, 2)
 RANGE_OTHER_CAPABILITY = (1, 2)
 RANGE_CLIMB_INHIBIT    = (0, 1)
+TCAS_PATH              = os.path.join(os.path.dirname(__file__), "..", "..", "src", "tcas.out")
+
+INDEX = {
+    "Cur_Vertical_Sep"          : 0,
+    "High_Confidence"           : 1,
+    "Two_of_Three_Reports_Valid": 2,
+    "Own_Tracked_Alt"           : 3,
+    "Own_Tracked_Alt_Rate"      : 4,
+    "Other_Tracked_Alt"         : 5,
+    "Alt_Layer_Value"           : 6,
+    "Up_Separation"             : 7,
+    "Down_Separation"           : 8,
+    "Other_RAC"                 : 9,
+    "Other_Capability"          : 10,
+    "Climb_Inhibit"             : 11
+}
+
+OTHER_RAC_VALUES = {
+    "NO_INTENT"     : 0,
+    "DO_NOT_CLIMB"  : 1,
+    "DO_NOT_DESCEND": 2
+}
+
+OTHER_CAPABILITY_VALUES = {
+    "TCAS_TA"       : 1,
+    "OTHER"         : 2
+}
 # ==========================================================
 # fmt:on
 
@@ -57,6 +89,48 @@ def gen_tcs_randomly(num: int):
         f.close()
 
     print("\nDone!")
+
+
+def load_test_cases(num: int) -> list:
+    """读取所有测试用例
+
+    Returns
+    -------
+    list
+        存储测试用例的列表
+    """
+    gen_tcs_randomly(num)
+
+    tcs_dir = os.path.join(os.path.dirname(__file__), "..", "..", "testcases")
+    tcs = list()
+
+    # 遍历存储测试用例的根目录, 依次读取文件
+    for file in os.listdir(tcs_dir):
+        with open(os.path.join(tcs_dir, file), "r") as f:
+            lst = [int(x) for x in f.read().split()]
+            tcs.append((lst,))
+
+    return tcs
+
+
+def run_TCAS(vals: list) -> str:
+    """运行 tcas.out 并返回输出
+
+    Parameters
+    ----------
+    vals : list
+        测试用例
+
+    Returns
+    -------
+    str
+        tcas.out 的输出
+    """
+    cmd_list = [TCAS_PATH]
+    tmp_list = [str(x) for x in vals]
+    cmd_list.extend(tmp_list)
+    output = subprocess.check_output(cmd_list, text=True).split("\n")
+    return output
 
 
 if __name__ == "__main__":
